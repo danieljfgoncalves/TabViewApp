aa6c3d243b9dc275d84a01aa05bddcbdef07b1f5
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-15 22:44:52 -0400

    Initial Commit

---
+++ TabViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabViewApp
+//
+//  Created by Daniel Goncalves on 2015-04-15.
+//  Copyright (c) 2015 Daniel J Goncalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabViewApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TabViewApp
+//
+//  Created by Daniel Goncalves on 2015-04-15.
+//  Copyright (c) 2015 Daniel J Goncalves. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
